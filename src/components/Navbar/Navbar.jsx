72385933be5521b8246ddd2f30909c8b75a58be4
Puzzles65 <chitrungnguyen12@gmail.com>
2023-08-14 05:33:17 +0200

    added links to navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,7 +2,8 @@ import '../Navbar/Navbar.scss'
 import { Link, NavLink } from 'react-router-dom'
 import logo from '../../assets/images/samplelogo.avif'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse, faAddressCard, faAddressBook, faGear, faFolderOpen, faFolder } from '@fortawesome/free-solid-svg-icons';
+import { faHouse, faUser, faAddressBook, faGear, faFolderOpen } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Navbar = () => (
     <div className='nav-bar'>
@@ -12,21 +13,33 @@ const Navbar = () => (
         </Link>
         <nav>
             <NavLink exact="true" activeclassname="active" to="/">
-                <FontAwesomeIcon icon={faHouse} color="orange" />
+                <FontAwesomeIcon icon={faHouse} color="#00ADB5" />
             </NavLink>
             <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
-                <FontAwesomeIcon icon={faAddressCard} color="orange" />
+                <FontAwesomeIcon icon={faUser} color="#00ADB5" />
             </NavLink>
             <NavLink exact="true" activeclassname="active" className="skills-link" to="/skills">
-                <FontAwesomeIcon icon={faGear} color="orange" />
+                <FontAwesomeIcon icon={faGear} color="#00ADB5" />
             </NavLink>
             <NavLink exact="true" activeclassname="active" className="projects-link" to="/projects">
-                <FontAwesomeIcon icon={faFolderOpen} color="orange" />
+                <FontAwesomeIcon icon={faFolderOpen} color="#00ADB5" />
             </NavLink>
             <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
-                <FontAwesomeIcon icon={faAddressBook} color="orange" />
+                <FontAwesomeIcon icon={faAddressBook} color="#00ADB5" />
             </NavLink>
         </nav>
+        <ul>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/chi-trung-nguyen/'>
+                    <FontAwesomeIcon icon={faLinkedin} color="#00ADB5" />
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://github.com/Puzzles65'>
+                    <FontAwesomeIcon icon={faGithub} color="#00ADB5" />
+                </a>
+            </li>
+        </ul>
     </div>
     
 )
