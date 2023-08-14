59d99091e9158cc1f7f837a98ec1989d751f5011
Puzzles65 <chitrungnguyen12@gmail.com>
2023-08-14 04:49:42 +0200

    navbar missing logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,8 @@
 import '../Navbar/Navbar.scss'
 import { Link, NavLink } from 'react-router-dom'
 import logo from '../../assets/images/samplelogo.avif'
-import { FontAwesomeIcon } from '@fortawesome/react-frontawesome'
-import { faHouse } from '@fortawesome/fontawesome-free-solid '
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHouse, faAddressCard, faAddressBook, faGear, faFolderOpen, faFolder } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = () => (
     <div className='nav-bar'>
@@ -12,7 +12,19 @@ const Navbar = () => (
         </Link>
         <nav>
             <NavLink exact="true" activeclassname="active" to="/">
-                <FontAwesomeIcon icon={faHouse} />
+                <FontAwesomeIcon icon={faHouse} color="orange" />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                <FontAwesomeIcon icon={faAddressCard} color="orange" />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="skills-link" to="/skills">
+                <FontAwesomeIcon icon={faGear} color="orange" />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="projects-link" to="/projects">
+                <FontAwesomeIcon icon={faFolderOpen} color="orange" />
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                <FontAwesomeIcon icon={faAddressBook} color="orange" />
             </NavLink>
         </nav>
     </div>
