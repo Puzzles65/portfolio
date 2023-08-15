844b0cf6c0b462ceb308dfdf513a6b664437a5b2
Puzzles65 <chitrungnguyen12@gmail.com>
2023-08-15 05:26:53 +0200

    contact me

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,3 +1,23 @@
-export default function Contact() {
-    return <h1>Contact</h1>
+import '../Contact/Contact.scss'
+import { FaEnvelope, FaGithub, FaLinkedin } from 'react-icons/fa';
+
+function Contact() {
+  return (
+    <section id="contact">
+      <h2>Contact Me</h2>
+      <div className="contact-links">
+        <a href="mailto:chitrungnguyen12@gmail.com">
+          <FaEnvelope />
+        </a>
+        <a href="https://github.com/Puzzles65" target="_blank" rel="noreferrer">
+          <FaGithub /> 
+        </a>
+        <a href="https://www.linkedin.com/in/chi-trung-nguyen/" target="_blank" rel="noreferrer">
+          <FaLinkedin /> 
+        </a>
+      </div>
+    </section>
+  );
 }
+
+export default Contact;
