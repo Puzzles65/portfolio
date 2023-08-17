a5828392bc8850631fd92913f48b41c919621a7c
Puzzles65 <chitrungnguyen12@gmail.com>
2023-08-18 01:07:59 +0200

    css changes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -17,6 +17,10 @@ const About = () => {
                     <p>
                         I am dedicated to transitioning from hospitality to tech, driven by my passion for creating exceptional experiences. With a strong customer service background and attention to detail, I bring a unique combination of interpersonal skills and technical knowledge. I thrive in fast-paced environments, love solving problems, and generating innovative ideas. Beyond my professional pursuits, my dedication to physical fitness and working out fuels my discipline, resilience, and commitment to personal growth. Through my teamwork, continuous learning, and maintaining a healthy lifestyle, I aim to positively impact the tech industry.
                     </p>
+                    <h3>Interests</h3>
+                    <p>
+                    Passionate about fitness, gaming, and cooking, I find joy in pushing my physical limits, exploring virtual worlds, and creating delicious dishes. Playing the guitar is my creative outlet, allowing me to express myself through music.
+                    </p>
                 </div>
             </div>
         </div>
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,48 +1,76 @@
 import '../Projects/Projects.scss'
-import Carousel from 'react-bootstrap/Carousel';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import Carousel from 'react-bootstrap/Carousel'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import projectData from '../../projectData'
 
 function Projects() {
-    return (
-        <div className="projects" id="projects">
-          <h2>Projects</h2>
-          <Carousel data-bs-theme="dark">
-            {projectData.map((project, index) => (
-              <Carousel.Item key={index}>
-                <div className="d-flex">
-                {project.link ? (
-              <a href={project.link} target="_blank" rel="noreferrer" className="image-container">
-                <img
-                  className="d-block w-100"
-                  src={project.image}
-                  alt={project.title}
-                />
-              </a>
-            ) : (
-              <div className="image-container">
-                <img
-                  className="d-block w-100"
-                  src={project.image}
-                  alt={project.title}
-                />
-              </div>
-            )}
-                  <div className="text-content">
-                    <h5>{project.title}</h5>
-                    <p>{project.description}</p>
+  return (
+    <div className="projects" id="projects">
+      <h2>Projects</h2>
+      <Carousel data-bs-theme="dark">
+        {projectData.map((project, index) => (
+          <Carousel.Item key={index}>
+            <div className="d-flex">
+              {project.link ? (
+                <a
+                  href={project.link}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="image-container"
+                >
+                  <img
+                    className="d-block w-100"
+                    src={project.image}
+                    alt={project.title}
+                  />
+                </a>
+              ) : (
+                <div className="image-container">
+                  <img
+                    className="d-block w-100"
+                    src={project.image}
+                    alt={project.title}
+                  />
+                </div>
+              )}
+              {project.githubLink ? (
+                <a
+                  href={project.githubLink}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="text-content"
+                >
+                  <h5>{project.title}</h5>
+                  <h3>{project.description}</h3>
+                  <div className='footer'>
                   <div className="icons-container">
                     {project.icons.map((Icon, iconIndex) => (
-                    <Icon key={iconIndex} className="icon" />
+                      <Icon key={iconIndex} className="icon" />
                     ))}
-                    </div>
+                  </div>
+                  <p>ReadMe</p>
+                  </div>
+                </a>
+              ) : (
+                <div className="text-content">
+                  <h5>{project.title}</h5>
+                  <h3>{project.description}</h3>
+                  <div className='footer'>
+                  <div className="icons-container">
+                    {project.icons.map((Icon, iconIndex) => (
+                      <Icon key={iconIndex} className="icon" />
+                    ))}
+                  </div>
+                  <p>ReadMe</p>
                   </div>
                 </div>
-              </Carousel.Item>
-            ))}
-          </Carousel>
-        </div>
-      );
-    }
+              )}
+            </div>
+          </Carousel.Item>
+        ))}
+      </Carousel>
+    </div>
+  )
+}
 
-export default Projects;
+export default Projects
